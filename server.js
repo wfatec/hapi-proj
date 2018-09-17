ff003f9acf5ae3a086e58ec061ed84b971dfe134
wfatec <chao125010@163.com>
2018-09-17 18:02:33 +0800

    add sqlite3

--- server.js
+++ server.js
@@ -1,53 +1,40 @@
 'use strict';
 
-const Bcrypt = require('bcrypt');
 const Hapi = require('hapi');
+const Sqlite3 = require('sqlite3');
 
-const users = {
-    john: {
-        username: 'john',
-        password: '$2a$10$iqJSHD.BGr0E2IxQwYgJmeP3NvhPrXAeLSaGCj6IR/XU5QtjVu5Tm',   // 'secret'
-        name: 'John Doe',
-        id: '2133d32a'
+const db = new Sqlite3.Database('./dindin.sqlite');
+
+const server = Hapi.server({
+    port: 3000,
+    host: 'localhost'
+});
+
+server.route([{
+    method: 'GET',
+    path: '/api/recipes',
+    handler: function (request, h) {
+
+        db.all('SELECT * FROM recipes', (err, results) => {
+
+            if (err) {
+                throw err;
+            }
+
+            return h.response(results);
+        });
     }
-};
+}]);
 
-const validate = async (request, username, password) => {
-
-    const user = users[username];
-    if (!user) {
-        return { credentials: null, isValid: false };
-    }
-
-    const isValid = await Bcrypt.compare(password, user.password);
-    const credentials = { id: user.id, name: user.name };
-
-    return { isValid, credentials };
-};
-
-const start = async () => {
-
-    const server = Hapi.server({ port: 4000 });
-
-    await server.register(require('hapi-auth-basic'));
-
-    server.auth.strategy('simple', 'basic', { validate });
-
-    server.route({
-        method: 'GET',
-        path: '/',
-        options: {
-            auth: 'simple'
-        },
-        handler: function (request, h) {
-
-            return 'welcome';
-        }
-    });
-
+const init = async () => {
     await server.start();
-
-    console.log('server running at: ' + server.info.uri);
+    console.log(`Server running at: ${server.info.uri}`);
 };
 
-start();
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+init();
