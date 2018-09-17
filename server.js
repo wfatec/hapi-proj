c445801af33c2978ea895719bb8c7b373b631d2e
wfatec <chao125010@163.com>
2018-09-17 23:56:44 +0800

    update server.js

--- server.js
+++ server.js
@@ -14,16 +14,52 @@ server.route([{
     method: 'GET',
     path: '/api/recipes',
     handler: function (request, h) {
-        const q = new Promise((resolve)=>{
-            db.all('SELECT * FROM recipes', (err, results) => {  
-                if (err) {
-                    throw err;
+        let sql = 'SELECT * FROM recipes';
+        const params = [];
+
+        if (request.query.cuisine) {
+            sql += ' WHERE cuisine = ?';
+            params.push(request.query.cuisine);
+        }
+
+        return new Promise((resolve)=>{
+            db.all(
+                sql,
+                params,
+                (err, results) => {  
+                    if (err) {
+                        throw err;
+                    }
+                    resolve(results)
                 }
-                resolve(results)
-            });
+            );
         })
-        
-        return q
+    }
+},{
+    method: 'GET',
+    path: '/api/recipes/{id}',
+    handler: function (request, h) {
+        return new Promise((resolve)=>{
+            db.get(
+                'SELECT * FROM recipes WHERE id = ?',
+                [request.params.id],
+                (err, results) => {
+
+                    if (err) {
+                        throw err;
+                    }
+
+                    resolve(results);
+                }
+            );
+        })
+        .then((results) => {
+            if (typeof results !== 'undefined') {
+                return results;
+            } else {
+                return h.response('Not found!').code(404)
+            }
+        })
     }
 }]);
 
