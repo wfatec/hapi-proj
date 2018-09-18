cad099bbd73a55b5b07824ce3142e8a9fb06f394
wfatec <chao125010@163.com>
2018-09-18 18:15:35 +0800

    add auth

---
+++ auth.js
@@ -0,0 +1,32 @@
+exports.api = async function(request, token, h){
+
+    // here is where you validate your token
+    // comparing with token from your database for example
+    new Promise((resolve)=>{
+        this.db.get(
+            'SELECT * FROM users WHERE token = ?',
+            [token],
+            (err, result) => {
+                if (err) {
+                    throw err;
+                }
+                resolve(result)
+            }
+        )
+        .then((user) => {
+            if (typeof user === 'undefined') {
+                return { isValid: false };
+            } else {
+                const isValid = true;
+                
+                const credentials = { token };
+                const artifacts = { 
+                    id: user.id,
+                    username: user.username
+                };
+            
+                return { isValid, credentials, artifacts };
+        })
+    }
+    
+}
--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 const Hapi = require('hapi');
 const Sqlite3 = require('sqlite3');
+const AuthBearer = require('hapi-auth-bearer-token');
 
 const db = new Sqlite3.Database('./dindin.sqlite');
 
@@ -10,11 +11,33 @@ const server = Hapi.server({
     host: 'localhost'
 });
 
-server.bind({ db : db });
-
-server.route(require('./routes'));
-
 const init = async () => {
+
+    server.bind({ db : db });
+
+    await server.register(AuthBearer)
+    
+    server.auth.strategy('simple', 'bearer-access-token', {
+        allowQueryToken: true,              // optional, false by default
+        validate: async (request, token, h) => {
+
+            // here is where you validate your token
+            // comparing with token from your database for example
+            const isValid = token === '1234';
+
+            const credentials = { token };
+            const artifacts = { test: 'info' };
+
+            return { isValid, credentials, artifacts };
+        }
+    });
+
+    server.auth.default('simple');
+
+    
+    
+    server.route(require('./routes'));
+
     await server.start();
     console.log(`Server running at: ${server.info.uri}`);
 };
